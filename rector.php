06fa7a2fb085e55ee843ccdde1fe06298182fe4d
Markus Welter | welante GmbH <markus@welante.ch>
2024-12-28 14:45:34 +0100

    php8 added rector

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/data/bin',
+        __DIR__ . '/lib',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0)
+    ->withSets([LevelSetList::UP_TO_PHP_83])
+;
